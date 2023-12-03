bd78f4f42eb23895da25de0208439c6654bc8614
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-12-03 16:43:36 +0300

    Moved guard && htmx middleware into sep. dir

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,27 @@
 #![doc = include_str!("../README.md")]
 #![forbid(unsafe_code)]
 
+/// Guard and htmx middleware.
+#[cfg(feature = "middleware")]
+#[cfg_attr(feature = "unstable", doc(cfg(feature = "middleware")))]
+pub mod middleware {
+    mod guard;
+    pub use guard::*;
+    mod htmx;
+    pub use htmx::*;
+}
+
 pub mod extractors;
-#[cfg(feature = "middleware")]
-#[cfg_attr(feature = "unstable", doc(cfg(feature = "middleware")))]
-pub mod guard;
 pub mod headers;
-#[cfg(feature = "middleware")]
-#[cfg_attr(feature = "unstable", doc(cfg(feature = "middleware")))]
-pub mod middleware;
 pub mod responders;
 
 #[doc(inline)]
 pub use extractors::*;
-#[cfg(feature = "middleware")]
-#[cfg_attr(feature = "unstable", doc(cfg(feature = "middleware")))]
-#[doc(inline)]
-pub use guard::*;
 #[doc(inline)]
 pub use headers::*;
-#[doc(inline)]
 #[cfg(feature = "middleware")]
 #[cfg_attr(feature = "unstable", doc(cfg(feature = "middleware")))]
+#[doc(inline)]
 pub use middleware::*;
 #[doc(inline)]
 pub use responders::*;
