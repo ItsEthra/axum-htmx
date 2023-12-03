b17cf6f95af64fdb43329cb5b58dc05fc3e20a95
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-12-03 21:41:45 +0300

    Improved example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,15 +1,18 @@
 use std::error::Error;
 
 use axum::{response::Html, routing::*, Router};
-use axum_htmx::middleware::{Htmx, HtmxLayer};
-use http::Uri;
+use axum_htmx::{
+    middleware::{Htmx, HtmxLayer},
+    SwapOption,
+};
 use tokio::{fs::read_to_string, net::TcpListener};
 
-async fn click(htmx: Htmx) -> Html<&'static str> {
-    htmx.res
-        .set_redirect("https://tokio.rs".parse::<Uri>().unwrap());
+async fn random(htmx: Htmx) -> Html<String> {
+    let num = fastrand::u32(0..100);
+    let name = htmx.req.prompt.unwrap_or_default();
+    htmx.res.set_reswap(SwapOption::AfterBegin);
 
-    Html("<p>Hello</p>")
+    Html(format!("<p>{name}: {num}</p>"))
 }
 
 async fn root() -> Html<String> {
@@ -20,7 +23,7 @@ async fn root() -> Html<String> {
 async fn main() -> Result<(), Box<dyn Error>> {
     let app = Router::new()
         .route("/", get(root))
-        .route("/click", get(click))
+        .route("/random", get(random))
         .layer(HtmxLayer::new());
     let listener = TcpListener::bind("127.0.0.1:9000").await?;
 
